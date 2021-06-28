ef606065a170d7a9d53a84ba6e0aee1e7d039c51
Chandan Kumar Jena <chandanjena706@gmail.com>
2021-06-28 10:35:25 +0530

    second commit

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -1,5 +1,10 @@
 package hello;
 public class Hello {
 	public static void main(String []args) {
+		if(4 > 5 ? true : false) {
+			System.out.println("chandan");
+		} else {
+			System.out.println("kumar");
+		}
 	}
 }
